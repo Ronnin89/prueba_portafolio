02bc4c36e78704d1f633bbec09ddc4b55d4bce6f
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2020-12-30 13:41:56 -0300

    portada/about

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("a").click(function(event){
+        if (this.hash !== "") {
+            event.preventDefault();
+            var scroll = this.hash;
+            $("html, body").animate({
+                scrollTop: $(scroll).offset().top
+            },800,function(){
+                window.location.hash = scroll;
+            });
+        }
+    });
+});
