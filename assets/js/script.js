74bdd67c67f599159ba37cccdf99bfa3c36ae5d0
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2020-12-30 16:55:30 -0300

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-$(function(){
-    $("a").click(function(event){
+$(function () {
+    $("a").click(function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             var scroll = this.hash;
             $("html, body").animate({
                 scrollTop: $(scroll).offset().top
-            },800,function(){
+            }, 800, function () {
                 window.location.hash = scroll;
             });
         }
     });
-});
+
+    var qs, js, q, s, d = document,
+        gi = d.getElementById,
+        ce = d.createElement,
+        gt = d.getElementsByTagName,
+        id = "typef_orm",
+        b = "https://embed.typeform.com/";
+    if (!gi.call(d, id)) {
+        js = ce.call(d, "script");
+        js.id = id;
+        js.src = b + "embed.js";
+        q = gt.call(d, "script")[0];
+        q.parentNode.insertBefore(js, q)
+    }
+})()
